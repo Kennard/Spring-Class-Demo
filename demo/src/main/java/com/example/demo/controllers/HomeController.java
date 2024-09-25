54188089473c054ea71f64b9967edd461af8745a
Kennard <kennard.mcgill@gatech.edu>
2024-09-25 11:13:41 -0400

    missing commits

--- demo/src/main/java/com/example/demo/controllers/HomeController.java
+++ demo/src/main/java/com/example/demo/controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+// URLS that are not API endpoints.
      @GetMapping("/")
      public String index() {
          return "index";
